f6aa4626a6eed398d8a088db07b00fe0f8fd51a5
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-03-04 18:22:40 +0530

    Card Grid Setup

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,53 @@
 import React from "react";
+import ProjectItem from "./ProjectItem.js";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default function Projects() {
   return (
-    <div className="container">
-      <h1>Projects</h1>
-    </div>
+    <>
+      <div
+        className="container d-flex flex-column justify-content-center my-5"
+        style={{ height: "100%" }}
+      >
+        <h1 className="text-center" style={{ marginTop: "10px" }}>
+          GitHub Projects
+        </h1>
+        <div
+          style={{
+            maxHeight: "85%",
+            overflowY: "auto",
+            padding: "15px",
+          }}
+        >
+          <InfiniteScroll
+            dataLength={10} //This is important field to render the next data
+            next={() => {}}
+            hasMore={false}
+            loader={<h4>Loading...</h4>}
+          >
+            <div className="container">
+              <div className="row gap-3 justify-content-center">
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+                <ProjectItem />
+              </div>
+            </div>
+          </InfiniteScroll>
+        </div>
+      </div>
+    </>
   );
 }
