9ee48befeedf76e108b1f6e923835e439abcedd4
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-03-04 18:44:37 +0530

    Card Foramtting

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -3,15 +3,37 @@ import placeholder from "../assets/400x200.png";
 export default function ProjectItem() {
   return (
     <div className="card" style={{ width: "18rem", padding: "15px" }}>
-      <img src={placeholder} className="card-img-top" alt="..." />
+      <img
+        src={placeholder}
+        className="card-img-top"
+        alt="..."
+        style={{ width: "100%", height: "30%" }}
+      />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+        <h5
+          className="card-title"
+          style={{ overflow: "hidden", height: "3rem" }}
+        >
+          Card title
+        </h5>
+        <p
+          className="card-text"
+          style={{
+            overflow: "hidden",
+            height: "7rem",
+            textAlign: "justify",
+            fontSize: "15px",
+          }}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
+          provident officia fugit repudiandae delectus eaque iusto, vel
+          necessitatibus dolore ad possimus excepturi voluptates doloremque
+          officiis amet, rem consequuntur odit? Cum, facere ipsa molestias eius
+          neque officia, sapiente modi eaque magni quibusdam ratione, totam ex.
+          Totam doloremque cupiditate in nemo sunt.
         </p>
         <a href="#" className="btn btn-primary">
-          Go somewhere
+          Open Project
         </a>
       </div>
     </div>
