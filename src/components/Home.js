008b5895d44948ea5b9a3236f407336104651b60
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-03-10 13:59:16 +0530

    Clean up

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,6 @@ import "./css/Home.css";
 import profilePhoto from "../assets/profile-photo.jpeg";
 import { ABOUT_ME_TITLE, ABOUT_ME_DATA } from "../Constants";
 import ParticleCanvas from "./ParticleCanvas.js";
-import Contact from "./Contact.js";
 
 export default function Home() {
   return (
@@ -16,7 +15,6 @@ export default function Home() {
             <h1 className="my-4">{ABOUT_ME_TITLE}</h1>
             <p className="my-4">{ABOUT_ME_DATA}</p>
             <br />
-            <Contact />
           </div>
           {/* Right Side: Image */}
           <div className="image-box" loading="lazy">
