e4c6c68edb0e9d37c90aceb3ef22212242fda6a2
Aayush Kamtikar <66491575+lostvayne47@users.noreply.github.com>
2025-03-18 13:36:40 +0530

    Padding added profile photo changed

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./css/Home.css";
-import profilePhoto from "../assets/profile-photo.jpeg";
+import profilePhotoLight from "../assets/home-assets-light.png";
+import profilePhotoDark from "../assets/home-assets-dark.png";
+
 import { ABOUT_ME_TITLE, ABOUT_ME_DATA } from "../Constants";
 import ParticleCanvas from "./ParticleCanvas.js";
+import portfolioContext from "../context/Context.js";
 
 export default function Home() {
+  const theme = useContext(portfolioContext);
   return (
     <>
       <ParticleCanvas />
@@ -12,13 +16,22 @@ export default function Home() {
         <section className="landing-section glass-effect">
           {/* Left Side: Text Content */}
           <div className="text-box">
-            <h1 className="my-4">{ABOUT_ME_TITLE}</h1>
+            <div className="my-4">
+              <h1 style={{ display: "inline" }}>Hi, I’m </h1>
+              <h1 style={{ display: "inline", color: "var(--name-colour)" }}>
+                {ABOUT_ME_TITLE}
+              </h1>
+            </div>
+
             <p className="my-4">{ABOUT_ME_DATA}</p>
             <br />
           </div>
           {/* Right Side: Image */}
           <div className="image-box" loading="lazy">
-            <img src={profilePhoto} alt="Landing Background" />
+            <img
+              src={theme === "dark" ? profilePhotoDark : profilePhotoLight}
+              alt="Landing Background"
+            />
           </div>
         </section>
       </div>
